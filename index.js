ab234eb589da9b50f681b373717bd82c22e06e4c
ismaelc <chris.ismael@gmail.com>
2016-06-23 14:27:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get('/graphCallback', function(request, response) {
 	getToken(request.query.code, function(err, data) {
 
 		body_response = data;
-		response.send(JSON.parse(data));
+		response.send(data);
 		//console.log(data);
 	});
 });
